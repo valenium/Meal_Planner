2cfc84b4ae352dea29d3f0520f756e70a791c95e
valenium <valenium@live.com>
2024-01-16 13:53:09 -0600

    update and delete routes for user

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, CollabGroup, Recipes, Meal
 
 # Register your models here.
 admin.site.register(CustomUser)
+admin.site.register(CollabGroup)
+admin.site.register(Recipes)
+admin.site.register(Meal)
